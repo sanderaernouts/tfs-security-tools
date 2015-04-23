862cb3dbdfef0b70004fba729a93dd085199ec48
sanderaernouts <sander.aernouts@gmail.com>
2015-04-23 10:28:17 +0200

    modified tool to generate CSV exportable information using the Export-Csv cmdlet

---
+++ TfsSecurityTools/TfsSecurityTools/Models/MembershipDescriptor.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TfsSecurityTools.Models
+{
+    public class MembershipDescriptor : TfsIdentityDescriptor
+    {
+        public string DisplayName { get; set; }
+
+        public Guid TeamFoundationId { get; set; }
+
+        public string Group { get; set; }
+        public Guid GroupId { get; set; }
+        public string Collection { get; set; }
+        public string CollectionUrl { get; set; }
+    }
+}
